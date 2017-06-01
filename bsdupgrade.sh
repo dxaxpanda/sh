eb08e9d828fd9692713776f818d2ace6e9443723
Jérémy MIRRE <jeremy.mirre@skores.com>
2017-06-01 18:13:44 +0200

    update upgrade script

--- bsdupgrade.sh
+++ bsdupgrade.sh
@@ -27,6 +27,18 @@ upgrade() {
       echo "Version 10.1-RELEASE est la prochaine mise à jour à effectuer pour "
       upgrade_version="10.1-RELEASE"
       freebsd-update upgrade -r $upgrade_version
+    elif [ $current_version == "10.1-RELEASE" ]; then
+      echo "Version 10.2-RELEASE est la prochaine mise à jour à effectuer pour "
+      upgrade_version="10.2-RELEASE"
+      freebsd-update upgrade -r $upgrade_version
+    elif [ $current_version == "10.2-RELEASE" ]; then
+      echo "Version 10.3-RELEASE est la prochaine mise à jour à effectuer pour "
+      upgrade_version="10.3-RELEASE"
+      freebsd-update upgrade -r $upgrade_version
+    else [ $current_version == "10.3-RELEASE" ]; then
+      echo "Version 11.0-RELEASE est la prochaine mise à jour à effectuer pour "
+      upgrade_version="11.0-RELEASE"
+      freebsd-update upgrade -r $upgrade_version
 
   esac
 
@@ -36,22 +48,28 @@ upgrade() {
 
 
 
-while [[ $family != jail || $family != host ]]; do
+while [ $family != jail || $family != host ]; do
 
-  case $family in 
+  case $family in
     jail*)
         echo "Le type de serveur choisi est [ ${family} ]."
         echo "Veuillez renseignez les noms des jails à mettre à jour."
-
-
+        ;;
     host*)
         echo "Le type de serveur choisi est [ ${family} ]."
         echo "Récolte des informations système pour la procédure de mise à jour..."
         current_version=$(freebsd-version)
         echo "La version actuelle de FreeBSD est là suivante : ${current_version}"
-        freebsd-update
+
+        fetch
+        install
+        upgrade
+        install
+        echo "Il est nécessaire de reboot la machine. a présent. Ensuite relancer un freebsd-update install."
+        ;;
     *)
       echo "Type de serveur mal renseigné veuillez recommencer."
+      ;;
     esac
 
 done
