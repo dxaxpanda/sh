e123aa39e043dfb972560134857c2771fca40ce2
Jérémy MIRRE <jeremy.mirre@skores.com>
2017-10-02 12:00:31 +0200

    modified exporter start script

--- prometheus_exporters/apache_exporter_start.sh
+++ prometheus_exporters/apache_exporter_start.sh
@@ -1,4 +1,7 @@
 
+
 #!/bin/sh
 
-su -m nobody -c "nohup /usr/local/bin/apache_exporter -telemetry.address "10.1.0.2:9117" >> apache_exporter.log 2>&1 &"
+su -m nobody -c "touch apache_exporter.log"
+su -m nobody -c "nohup /usr/local/bin/apache_exporter -scrape_uri 'http://localhost/server-status?auto' -telemetry.address '10.1.0.2:9117' >> apache_exporter.log 2>&1 &"
+
