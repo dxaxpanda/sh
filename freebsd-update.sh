35baf39bea88966f85f8d3cbf7d332bb0f41bb74
dxaxpanda <jmmirre@gmail.com>
2017-10-03 11:03:37 +0200

    Update freebsd-update.sh

--- freebsd-update.sh
+++ freebsd-update.sh
@@ -2,19 +2,14 @@
 
 # This script partially automate the update of freebsd machines
 
+JAIL_NAME=$1
+VERSION=$2
+UPGRADE=$3
 
+freebsd-update -b /jails/${JAIL_NAME} --currently-running ${VERSION}-RELEASE fetch 
+freebsd-update -b /jails/${JAIL_NAME} --currently-running ${VERSION}-RELEASE install
+freebsd-update -b /jails/${JAIL_NAME} --currently-running ${VERSION}-RELEASE -r ${UPGRADE}-RELEASE upgrade
+freebsd-update -b /jails/${JAIL_NAME} --currently-running ${VERSION}-RELEASE install
+jail -rc ${JAIL_NAME} 
+freebsd-update -b /jails/${JAIL_NAME} --currently-running ${VERSION}-RELEASE install
 
-
-
-
-
-
-freebsd-update --not-running-from-cron fetch
-
-freebsd-update --not-running-from-cron install
-
-freebsd-update --not-running-from-cron -r $1-RELEASE upgrade
-
-freebsd-update --not-running-from-cron install
-
-freebsd-update --not-running-from-cron install
