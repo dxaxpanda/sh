078634a78ed78d80b0962fbdd13565b52be0ce96
Jérémy MIRRE <jeremy.mirre@skores.com>
2017-10-05 15:12:35 +0200

    upgrade and zfs on root script improved

--- freebsd-update.sh
+++ freebsd-update.sh
@@ -2,14 +2,11 @@
 
 # This script partially automate the update of freebsd machines
 
-JAIL_NAME=$1
-VERSION=$2
-UPGRADE=$3
+VERSION=$1
+UPGRADE=$2
 
-freebsd-update -b /jails/${JAIL_NAME} --currently-running ${VERSION}-RELEASE fetch 
-freebsd-update -b /jails/${JAIL_NAME} --currently-running ${VERSION}-RELEASE install
-freebsd-update -b /jails/${JAIL_NAME} --currently-running ${VERSION}-RELEASE -r ${UPGRADE}-RELEASE upgrade
-freebsd-update -b /jails/${JAIL_NAME} --currently-running ${VERSION}-RELEASE install
-jail -rc ${JAIL_NAME} 
-freebsd-update -b /jails/${JAIL_NAME} --currently-running ${VERSION}-RELEASE install
+freebsd-update --currently-running ${VERSION}-RELEASE fetch 
+freebsd-update --currently-running ${VERSION}-RELEASE install
+freebsd-update --currently-running ${VERSION}-RELEASE -r ${UPGRADE}-RELEASE upgrade
+freebsd-update --currently-running ${VERSION}-RELEASE install
 
